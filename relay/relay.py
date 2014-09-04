2da510805765f1b427c86740731c6f2437acbd1e
Carter Hinsley <carterhinsley@gmail.com>
2014-09-04 19:35:02 -0400

    relay prototyped

--- relay/relay.py
+++ relay/relay.py
@@ -3,6 +3,7 @@
 # LeanDNS Relay
 # relay.py
 
+import json
 import socket
 import threading
 
@@ -16,24 +17,53 @@ sock.listen(1)
 
 domains = {}
 servers = {}
+clients = {}
 threads = {}
 
 def handleConnection(conn, addr):
 	try:
 		print("Connection from ", addr)
+		client_type = ""
 		while (True):
-			data = conn.recv(1024)
-			print(data)
-			if (data.endswith(b"\n")):
-				break
+			data = ""
+			while (True):
+				data += conn.recv(512).decode("utf-8")
+				if (data.endswith("\n")):
+					break
+			for line in [line for line in data.split("\n") if line != ""]:
+				print(line)
+				msg = json.loads(line)
+				if (msg["type"] == "client info"):
+					client_type = msg["client type"]
+					if (client_type == "server"):
+						servers[conn] = set()
+					if (client_type == "client"):
+						clients[conn] = []
+				elif (msg["type"] == "quit"):
+					break
+				elif (client_type == "server"):
+					if (msg["type"] == "domain request"):
+						for domain in msg["domains"]:
+							(domain_name, domain_addr) = domain
+							servers[conn].update({domain_name})
+							domains[domain_name] = {"addr": domain_addr, "owner": addr}
+						print("Domains: %s" % repr(domains))
+						print("Servers: %s" % repr(servers))
+				elif (client_type == "client"):
+					pass
 	finally:
+		if conn in domains:
+			del domains[conn]
+		if conn in servers:
+			del servers[conn]
+		if conn in clients:
+			del clients[conn]
 		conn.close()
 
 print("Listening for connections...")
 try:
 	while (True):
 		(conn, client_addr) = sock.accept()
-		servers[conn] = []
 		threads[conn] = threading.Thread(target=handleConnection, args=(conn, client_addr))
 		threads[conn].start()
 finally:
