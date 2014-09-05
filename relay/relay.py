c023edb46a5a06ac5d14bb5710a485b9167509a5
Carter Hinsley <carterhinsley@gmail.com>
2014-09-05 14:20:24 -0400

    pinging with timeouts and send()/recv()

--- relay/relay.py
+++ relay/relay.py
@@ -4,8 +4,10 @@
 # relay.py
 
 import json
+import random
 import socket
 import threading
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -20,17 +22,38 @@ servers = {}
 clients = {}
 threads = {}
 
+def send(conn, data):
+	conn.send((json.dumps(data) + "\n").encode("utf-8"))
+
+def recv(conn):
+	data = ""
+	while (True):
+		data += conn.recv(512).decode("utf-8")
+		if (data.endswith("\n")):
+			return data
+
 def handleConnection(conn, addr):
+	running = True
+	def ping(ping_pending):
+		if (ping_pending[0]):
+			raise IOError("ping timeout")
+		print("pinging...")
+		identifier = random.randint(10000, 99999)
+		send(conn, {"type": "ping", "id": identifier})
+		ping_pending[0] = identifier
+	def pingLoop(ping_pending):
+		while (running):
+			ping(ping_pending)
+			time.sleep(180)
 	try:
 		print("Connection from ", addr)
+		ping_pending = [False] # Must be mutable
+		print(ping_pending)
+		ping_thread = threading.Thread(target=pingLoop, args=[ping_pending])
+		ping_thread.start()
 		client_type = ""
-		while (True):
-			data = ""
-			while (True):
-				data += conn.recv(512).decode("utf-8")
-				if (data.endswith("\n")):
-					break
-			for line in [line for line in data.split("\n") if line != ""]:
+		while (running):
+			for line in [line for line in recv(conn).split("\n") if line != ""]:
 				print(line)
 				msg = json.loads(line)
 				if (msg["type"] == "client info"):
@@ -39,8 +62,12 @@ def handleConnection(conn, addr):
 						servers[conn] = set()
 					if (client_type == "client"):
 						clients[conn] = []
+				elif (msg["type"] == "pong"):
+					pass
+					if (ping_pending[0] and ping_pending[0] == msg["id"]):
+						ping_pending[0] = False
 				elif (msg["type"] == "quit"):
-					break
+					running = False
 				elif (client_type == "server"):
 					if (msg["type"] == "domain request"):
 						for domain in msg["domains"]:
@@ -52,6 +79,7 @@ def handleConnection(conn, addr):
 				elif (client_type == "client"):
 					pass
 	finally:
+		ping_thread.join()
 		if conn in domains:
 			del domains[conn]
 		if conn in servers:
