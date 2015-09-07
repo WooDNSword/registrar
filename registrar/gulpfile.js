41ca9b72efc5b4db81f18ccee210c63f3548d71a
Carter Hinsley <carterhinsley@gmail.com>
2015-09-07 16:57:23 -0400

    Add 'gulp build-coffee' command

--- registrar/gulpfile.js
+++ registrar/gulpfile.js
@@ -1,4 +1,11 @@
 var
     coffee = require('gulp-coffee'),
-    gulp   = require('gulp');
+    gulp   = require('gulp'),
+    gutil  = require('gulp-util');
+
+gulp.task('build-coffee', function () {
+    gulp.src('./src/*.coffee')
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest('./dist/'));
+});
 
