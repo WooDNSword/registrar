412c70312838b8b690142f4bc5acd098d910d6cf
Carter Hinsley <carterhinsley@gmail.com>
2015-09-07 18:19:32 -0400

    Add 'gulp build' command

--- registrant/gulpfile.js
+++ registrant/gulpfile.js
@@ -3,6 +3,10 @@ var
     gulp   = require('gulp'),
     gutil  = require('gulp-util');
 
+gulp.task('build', [
+    'build-coffee'
+]);
+
 gulp.task('build-coffee', function () {
     gulp.src('./src/*.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
