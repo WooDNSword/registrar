edfaf882e4c31ee91f5cdd4cdacc3b1c66d0fea7
Carter Hinsley <carterhinsley@gmail.com>
2015-09-07 18:34:09 -0400

    Add 'gulp build-resources' command

--- registrant/gulpfile.js
+++ registrant/gulpfile.js
@@ -4,12 +4,18 @@ var
     gutil  = require('gulp-util');
 
 gulp.task('build', [
-    'build-coffee'
+    'build-coffee',
+    'build-resources'
 ]);
 
 gulp.task('build-coffee', function () {
     gulp.src('./src/*.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
         .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build-resources', function () {
+    return gulp.src('./src/res/**/*')
+        .pipe(gulp.dest('./dist/res/'));
 });
 
