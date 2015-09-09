6b62e9d86b78ea66e5b568654df07c9a92c63ad5
Carter Hinsley <carterhinsley@gmail.com>
2015-09-09 10:47:23 -0400

    Add coffeescript testing for registrant

--- registrant/gulpfile.js
+++ registrant/gulpfile.js
@@ -6,13 +6,20 @@ var
 
 gulp.task('build', [
     'build-coffee',
+    'build-coffee-tests',
     'build-resources'
 ]);
 
 gulp.task('build-coffee', function () {
-    gulp.src('./src/**/*.coffee')
+    return gulp.src('./src/**/*.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
         .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build-coffee-tests', function () {
+    return gulp.src('./test/src/**/*.coffee')
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+	.pipe(gulp.dest('./test/dist/'));
 });
 
 gulp.task('build-resources', function () {
@@ -23,6 +30,8 @@ gulp.task('build-resources', function () {
 gulp.task('default', function () {
     // Watch for CoffeeScript changes
     gulp.watch('./src/**/*.coffee', ['build-coffee']);
+    // Watch for CoffeeScript test changes
+    gulp.watch('./test/src/**/*.coffee', ['build-coffee-tests']);
     // Watch for resource changes
     gulp.watch('./src/res/**/*', ['build-resources']);
 });
