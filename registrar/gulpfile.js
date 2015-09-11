686be72b27c7e0f10026db8273669fab9acb1836
Carter Hinsley <carterhinsley@gmail.com>
2015-09-11 11:25:20 -0400

    Translate tests to LiveScript

--- registrar/gulpfile.js
+++ registrar/gulpfile.js
@@ -2,11 +2,15 @@ var
     coffee     = require('gulp-coffee'),
     coffeelint = require('gulp-coffeelint'),
     gulp       = require('gulp'),
-    gutil      = require('gulp-util');
+    gutil      = require('gulp-util'),
+    livescript = require('gulp-livescript')
+    lslint     = require('gulp-lint-ls');
 
 gulp.task('build', [
     'build-coffee',
     'build-coffee-tests',
+    'build-livescript',
+    'build-livescript-tests',
     'build-resources'
 ]);
 
@@ -22,6 +26,18 @@ gulp.task('build-coffee-tests', function () {
         .pipe(gulp.dest('./test/dist/'));
 });
 
+gulp.task('build-livescript', function () {
+    return gulp.src('./src/**/*.ls')
+        .pipe(livescript({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build-livescript-tests', function () {
+    return gulp.src('./test/src/**/*.ls')
+        .pipe(livescript({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest('./test/dist/'));
+});
+
 gulp.task('build-resources', function () {
     return gulp.src('./src/res/**/*')
         .pipe(gulp.dest('./dist/res/'));
@@ -32,17 +48,23 @@ gulp.task('default', function () {
     gulp.watch('./src/**/*.coffee', ['build-coffee']);
     // Watch for CoffeeScript test changes
     gulp.watch('./test/src/**/*.coffee', ['build-coffee-tests']);
+    // Watch for LiveScript changes
+    gulp.watch('./src/**/*.ls', ['build-livescript']);
+    // Watch for LiveScript test changes
+    gulp.watch('./test/src/**/*.ls', ['build-livescript-tests']);
     // Watch for resource changes
     gulp.watch('./src/res/**/*', ['build-resources']);
 });
 
 gulp.task('lint', [
     'lint-coffee',
-    'lint-coffee-tests'
+    'lint-coffee-tests',
+    'lint-livescript',
+    'lint-livescript-tests'
 ]);
 
 gulp.task('lint-coffee', function () {
-    return gulp.src('./src/scripts/**/*.coffee')
+    return gulp.src('./src/**/*.coffee')
         .pipe(coffeelint())
         .pipe(coffeelint.reporter());
 });
@@ -53,3 +75,12 @@ gulp.task('lint-coffee-tests', function () {
         .pipe(coffeelint.reporter());
 });
 
+gulp.task('lint-livescript', function () {
+    return gulp.src('./src/**/*.ls')
+        .pipe(lslint());
+});
+
+gulp.task('lint-livescript-tests', function () {
+    return gulp.src('./test/src/**/*.ls')
+        .pipe(lslint());
+});
---
+++ registrar/test/src/connection_util.ls
@@ -0,0 +1,45 @@
+assert = require 'assert'
+conn-util = require '../../dist/connection/util'
+
+describe 'connection/util', ->
+    describe '.endpoint-to-string', ->
+        it 'should produce correct output', (done) ->
+            assert.equal(
+                conn-util.endpoint-to-string(
+                    host: '127.0.0.1'
+                    port: 8080
+                ),
+                '127.0.0.1:8080'
+            )
+
+            assert.equal(
+                conn-util.endpoint-to-string(
+                    host: 'localhost'
+                    port: 6667
+                ),
+                'localhost:6667'
+            )
+
+            done!
+
+    describe '.string-to-endpoint', ->
+        it 'should produce a valid endpoint object', (done) ->
+            assert.deepEqual(
+                (conn-util.string-to-endpoint '127.0.0.1:8080'),
+                host: '127.0.0.1'
+                port: 8080
+            )
+
+            assert.deepEqual(
+                (conn-util.string-to-endpoint 'localhost:6667'),
+                host: 'localhost'
+                port: 6667
+            )
+
+            done!
+
+        it 'should produce NaN when an invalid port number is provided', (done) ->
+            (assert.ok . isNaN) (conn-util.string-to-endpoint '127.0.0.1:invalidPort').port
+
+            done!
+    
