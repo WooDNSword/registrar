bbe448926bc9b8a5c940f3e2f20bc89b1a2de5af
Carter Hinsley <carterhinsley@gmail.com>
2015-11-01 09:21:34 -0500

    Translate connection_util test module (ls->js)
    Although variable names were changed from lisp-case to camelCase,
    LiveScript originally translated lisp-case to camelCase upon
    compilation to JavaScript, so the two files are now effectively
    equivalent.

---
+++ test/src/connection_util.js
@@ -0,0 +1,58 @@
+var assert = require('assert'),
+    connUtil = require('../../dist/connection/util');
+
+describe('connection/util', function () {
+    describe('.endpointToString', function () {
+        it('should produce correct output', function (done) {
+            assert.equal(
+                connUtil.endpointToString({
+                    'host': '127.0.0.1',
+                    'port': 8080
+                }),
+                '127.0.0.1:8080'
+            );
+
+            assert.equal(
+                connUtil.endpointToString({
+                    'host': 'localhost',
+                    'port': 6667
+                }),
+                'localhost:6667'
+            );
+
+            return done();
+        });
+    });
+
+    describe('.stringToEndpoint', function () {
+        it('should produce a valid endpoint object', function (done) {
+            assert.deepEqual(
+                {
+                    'host': '127.0.0.1',
+                    'port': 8080
+                },
+                connUtil.stringToEndpoint('localhost:6667')
+            );
+
+            assert.deepEqual(
+                {
+                    'host': 'localhost',
+                    'port': 6667
+                },
+                connUtil.stringToEndpoint('localhost:6667')
+            );
+
+            return done();
+        });
+
+        it('should produce NaN when an invalid port number is provided', function (done) {
+            assert.ok(
+                isNaN(
+                    connUtil.stringToEndpoint('127.0.0.1:invalidPort').port
+                )
+            );
+
+            return done();
+        });
+    });
+});
