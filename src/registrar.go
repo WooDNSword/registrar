79ddb421e5820b7ca50fd10092ac564cc2250d85
Carter Hinsley <carterhinsley@outlook.com>
2015-11-07 03:52:15 +0000

    Add basic server logic

--- src/registrar.go
+++ src/registrar.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
+func handleConnection(conn net.Conn) {
+	conn.Write([]byte("Hello, client!"))
+	conn.Close()
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	port := "55555"
+
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println("Listening on port", port)
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// Handle error
+		}
+		go handleConnection(conn)
+	}
 }
