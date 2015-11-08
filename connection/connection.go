26cd2e4929129b87f1b2af625ff931f815d54213
Carter Hinsley <carterhinsley@outlook.com>
2015-11-08 17:18:51 +0000

    Use block comments for comments longer than 3 lines

--- connection/connection.go
+++ connection/connection.go
@@ -7,19 +7,23 @@ import (
 	"net"
 )
 
-// HandleConnection spawns an instance of a supplied `connectionHandler`
-// function, which it supplies a net.Conn object `conn` to, and closes the
-// connection afterwards to ensure that it gets closed even if
-// `connectionHandler` does not close it.
+/*
+HandleConnection spawns an instance of a supplied `connectionHandler`
+function, which it supplies a net.Conn object `conn` to, and closes the
+connection afterwards to ensure that it gets closed even if
+`connectionHandler` does not close it.
+*/
 func HandleConnection(connectionHandler func(net.Conn), conn net.Conn) {
 	defer conn.Close()
 	connectionHandler(conn)
 }
 
-// Initiate creates a listener on the specified `port` and begins a loop
-// spawning off instances of the supplied function `connectionHandler` with the
-// helper function `HandleConnection` for each connection established by the
-// listener.
+/*
+Initiate creates a listener on the specified `port` and begins a loop
+spawning off instances of the supplied function `connectionHandler` with the
+helper function `HandleConnection` for each connection established by the
+listener.
+*/
 func Initiate(port string, connectionHandler func(net.Conn)) {
 	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
--- session/session.go
+++ session/session.go
@@ -6,10 +6,12 @@ import (
 	"net"
 )
 
-// Handler accepts a client connection and operates on it; this function is used
-// to define how clients are handled post-connection and pre-close. A helper
-// function handles connection object closing, so this function should not close
-// the connection itself.
+/*
+Handler accepts a client connection and operates on it; this function is used
+to define how clients are handled post-connection and pre-close. A helper
+function handles connection object closing, so this function should not close
+the connection itself.
+*/
 func Handler(conn net.Conn) {
 	conn.Write([]byte("Greetings, client!"))
 }
