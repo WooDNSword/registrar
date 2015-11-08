bd319e098e24d1927aee0039ac1d2b37a61aea5b
Carter Hinsley <carterhinsley@outlook.com>
2015-11-08 17:14:41 +0000

    Add comment for session.Handler function

--- session/session.go
+++ session/session.go
@@ -6,6 +6,10 @@ import (
 	"net"
 )
 
+// Handler accepts a client connection and operates on it; this function is used
+// to define how clients are handled post-connection and pre-close. A helper
+// function handles connection object closing, so this function should not close
+// the connection itself.
 func Handler(conn net.Conn) {
 	conn.Write([]byte("Greetings, client!"))
 }
