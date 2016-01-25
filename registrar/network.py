3e569193a925522180803a83f48caa3bc85f87fd
Carter Hinsley <carterhinsley@outlook.com>
2016-01-25 19:43:56 +0000

    Add Hello World in Haskell
    Next up: Build & testing systems.

--- registrar/network.py
+++
@@ -1,8 +0,0 @@
-# TODO: Document the module!
-# TODO: Move this module into an independent repository (DRY principle: more
-# than one repository uses equivalent code.
-
-registrant_name = 'registrant'
-registrar_name = 'registrar'
-resolver_name = 'resolver'
-unidentified_client_name = 'unidentified'
